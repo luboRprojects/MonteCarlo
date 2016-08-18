f17a200d77518913eb5c35fbf223441e7e4ef001
lubor <homolkalubor@seznam.cz>
2016-08-18 10:26:09 +0700

    Sampling through lists

--- model_build.R
+++ model_build.R
@@ -8,6 +8,7 @@ a1_prob <- grep(x=assumptions[[1]]$Variable, pattern="_P")
 a1_vals <- which(!(1:nrow(assumptions[[1]]) %in% a1_prob))
 
 a1 <- data.frame(assumptions[[1]][a1_vals, ], assumptions[[1]][a1_prob, ])
+a2 <- data.frame(assumptions[[2]][a1_vals, ], assumptions[[2]][a1_prob, ])
 #---- Constants -----
 # TODO: read from external file
 shares <- rep(15119946, 5)
@@ -18,3 +19,30 @@ sell_price_0 <- c(91500, 83000,  134578,  114929,  108794)
 rev_init <- c(8206, 3648, 27286, 37757, 129710)
 
 unique(assumptions[[1]]$Variable)
+
+#---- Compute Simulated values - Mines ----
+n.boot <- 50
+
+a1.2 <- a1 %>% group_by(Mine, Year, Variable) %>% select(Value, Value.1) %>%
+ mutate(
+  class = as.factor(paste0(Variable, Year, Mine)),
+  prob = Value.1/100
+  ) %>% ungroup() %>% split(.,.$class)
+
+a2.2 <- a1 %>% group_by(Year, Variable) %>% select(Value, Value.1) %>%
+ mutate(
+  class = as.factor(paste0(Variable, Year)),
+  prob = Value.1/100
+  ) %>% ungroup() %>% split(.,.$class)
+
+mc_values.1 <- lapply(a1.2, function(x){
+ sample(x$Value, size=n.boot, replace=TRUE, prob=x$prob)
+ })
+
+mc_values.2 <- lapply(a2.2, function(x){
+ sample(x$Value, size=n.boot, replace=TRUE, prob=x$prob)
+ })
+
+
+
+
